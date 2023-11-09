e544472bc554eb2067617b8b40fa7eabcffd250a
oletipravallika <136584738+oletipravallika@users.noreply.github.com>
2023-11-09 12:55:42 +0530

    Create index.tf

---
+++ index.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+ 
+ 
+resource "aws_instance" "Test-Server" {
+  ami           = "ami-05c13eab67c5d8861"
+  instance_type = "t2.micro"
+  tags = {
+Name = "pravallika-Server"
+}
+}
